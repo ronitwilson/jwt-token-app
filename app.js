81a60e857395a94f9e506356d7ad1395f78431fa
Ronit Wilson <ronit.study@wsl>
2023-09-30 16:53:56 +0530

    add starter code correctly

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ require('express-async-errors');
 const express = require('express');
 const app = express();
 
-const mainRouter = require('./routes/main');
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
@@ -12,12 +11,10 @@ const errorHandlerMiddleware = require('./middleware/error-handler');
 app.use(express.static('./public'));
 app.use(express.json());
 
-app.use('/api/v1', mainRouter);
-
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
